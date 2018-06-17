0dc5596460946a8a53ab9892d453571b3649f748
brutusk94 <40342042+brutusk94@users.noreply.github.com>
2018-06-17 15:26:43 -0400

    P2T1_SalesPrediction
    My first submission in GitHub

---
+++ P2T1_SalesPrediction_BrutusKenric.py
@@ -0,0 +1,12 @@
+# CTI-110
+# P2T1 - Sales Prediction
+# Name
+# Date
+# Get the projected total sales.
+total_sales = float(input("Enter the projected sales: "))
+
+# Calculate the profits as 23 percent of total sales.
+profit = total_sales * 0.23
+
+# Display the profit.
+print("The profit is $", format(profit, ",.2f"))
